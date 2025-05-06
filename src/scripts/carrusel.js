f7048f13cad2c8291b08ac874dc3465d193ac78d
diego <diego@legolas.dcaa.unam.mx>
2025-05-06 15:07:19 -0600

    cambios de carrusel

--- src/scripts/carrusel.js
+++ src/scripts/carrusel.js
@@ -8,7 +8,17 @@ export function initCarrusel(){
 
   function updateCarousel() {
     const offset = -currentIndex * 100;
-    slidesContainer.style.transform = `translateX(${offset}%)`;
+   slidesContainer.style.transform = `translateX(${offset}%)`;
+
+    for(var i = 0; i<totalSlides; i++){
+      var noSelect = document.getElementById(i)
+      noSelect.style.backgroundColor = "#EAEAEA"
+      console.log(i)        
+    }
+
+
+    var elegida = document.getElementById(currentIndex)
+    elegida.style.backgroundColor = `#B7DDFA`;
   }
 
   document.getElementById('prevBtn').addEventListener('click', () => {
@@ -21,4 +31,26 @@ export function initCarrusel(){
     updateCarousel();
   });
 
+
+
+
+  document.getElementById('0').addEventListener('click', () => {
+    currentIndex = (0) % totalSlides;
+    updateCarousel();
+  });
+  document.getElementById('1').addEventListener('click', () => {
+    currentIndex = (1) % totalSlides;
+    updateCarousel();
+  });
+  document.getElementById('2').addEventListener('click', () => {
+    currentIndex = (2) % totalSlides;
+    updateCarousel();
+  });
+  document.getElementById('3').addEventListener('click', () => {
+    currentIndex = (3) % totalSlides;
+    updateCarousel();
+  });
+
+
 }
+
