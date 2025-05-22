45e7ec92e38a60028b80c53d0324952e5abd236d
diego <diego@legolas.dcaa.unam.mx>
2025-05-22 12:23:57 -0600

    Cambio de imagenes jpg a webp y color de las tarjetas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
-    applyBaseStyles: false,
-  }),]
-
+    applyBaseStyles: false
+  }), sitemap()]
 });
